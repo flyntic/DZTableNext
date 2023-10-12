02ed73f5665565cb764a58d45e7e8da392584997
flyntic <64376713+flyntic@users.noreply.github.com>
2023-10-12 17:05:07 +0300

    Update user.jsx

--- components/user.jsx
+++ components/user.jsx
@@ -1,4 +1,4 @@
-function ToStringObj(_obj,edit,save)
+function ToStringObj(_obj,edit,save,number="")
 {
   let obj=_obj;  
 
@@ -8,20 +8,20 @@ function ToStringObj(_obj,edit,save)
      {
       if (!edit)  return obj;
       return  <input type="text" value={obj} class="editing"
-                     onChange={(evt)=>{obj=save(evt.target.value);}}/>
+                     onChange={(evt)=>{obj=save(evt.target.value,number);}}/>
      }
      else 
       {
         let keys= Object.keys(obj);
-        if (!edit)
+       // if (!edit)
          return (
           <>{
-            keys.map((i)=>{
-              return <div>{i}:{obj[i]}</div>
-             })
+            keys.map((i)=> {return <div>{i}: {ToStringObj(obj[i],edit,save,number.concat(".",i))}</div>}
+             // return <div>{i}:{obj[i]}</div>
+             )
             }</>
          );
-        else 
+    /*    else 
         return (
           <>{
             keys.map((i)=>{
@@ -33,7 +33,7 @@ function ToStringObj(_obj,edit,save)
              })
             }</>
          );
-
+*/
       }
     }
  
@@ -51,8 +51,13 @@ export function User({user,Actions})
   for (let i=0;i<keys.length;i++) 
         {
           let value=ToStringObj(user[keys[i]],(user.id!=0) && (user.id==indexEdit),
-                               (obj,n=0)=>{if (n==0) {user[keys[i]]=obj;Actions.Save(user);return user[keys[i]];}
-                                              user[keys[i]][n]=obj;Actions.Save(user);return user[keys[i]][n];
+                               (obj,n="")=>{  if (n=="") {user[keys[i]]=obj;Actions.Save(user);return user[keys[i]];}
+                                              let str_n=n.split("."); 
+                                              
+                                              if (str_n.length==2)
+                                                 {console.log(str_n,keys[i]); user[keys[i]][str_n[1]]=obj;Actions.Save(user);return user[keys[i]][str_n[1]];}
+                                              else   
+                                                 {user[keys[i]][str_n[1]][str_n[2]]=obj;Actions.Save(user);return user[keys[i]][str_n[1]][str_n[2]];}
                                            });    
           console.log(value);      
           cols.push({index:i, colname:keys[i], value:value});
