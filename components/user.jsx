a4765ff4a96e6ce2d37103c3851ef70b94a0e577
flyntic <64376713+flyntic@users.noreply.github.com>
2023-10-12 13:54:21 +0300

    Update user.jsx

--- components/user.jsx
+++ components/user.jsx
@@ -1,19 +1,42 @@
-function ToStringObj(obj)
+function ToStringObj(_obj,edit,save)
 {
-  let str="";
+  let obj=_obj;  
 
   if(obj!==0)  
    { 
      if(( Object.keys(obj)==0) || (Object.keys(obj)[0]==0 ))
      {
-        str=obj;
+      if (!edit)  return obj;
+      return  <input type="text" value={obj} class="editing"
+                     onChange={(evt)=>{obj=save(evt.target.value);}}/>
      }
      else 
-      {Object.keys(obj).forEach(k=> str=str.concat(k,":",obj[k]," "));
-        
+      {
+        let keys= Object.keys(obj);
+        if (!edit)
+         return (
+          <>{
+            keys.map((i)=>{
+              return <div>{i}:{obj[i]}</div>
+             })
+            }</>
+         );
+        else 
+        return (
+          <>{
+            keys.map((i)=>{
+              return <div>
+                        <div>{i}:</div>
+                        <input type="text" value={obj[i]} class="editing"
+                                  onChange={(evt)=>{obj[i]=save(evt.target.value,i);}}/>
+                      </div>
+             })
+            }</>
+         );
+
       }
     }
-  return str;
+ 
  }
 
 
@@ -27,23 +50,24 @@ export function User({user,Actions})
 
   for (let i=0;i<keys.length;i++) 
         {
-          let value=ToStringObj(user[keys[i]]);    
+          let value=ToStringObj(user[keys[i]],(user.id!=0) && (user.id==indexEdit),
+                               (obj,n=0)=>{if (n==0) {user[keys[i]]=obj;Actions.Save(user);return user[keys[i]];}
+                                              user[keys[i]][n]=obj;Actions.Save(user);return user[keys[i]][n];
+                                           });    
           console.log(value);      
           cols.push({index:i, colname:keys[i], value:value});
          } ;
 
-  // console.log(Actions);   
+ 
   if (user.id==indexEdit)
   return (
     <>
     <tr>
-      {
-         cols.map((i)=> { if(i.index!=0)
-          {  <td><input type="text" value={user[i.colname]} class="editing"
-                               onChange={(evt)=>{user[i.colname]=evt.target.value;Actions.Save(user)}}/> </td>     } 
-             else  <td ClassName={i.colname} >{i.value}</td>
-              })
-            }
+        { cols.map((i)=> 
+        { 
+           return  <td ClassName={i.colname}  >{i.value}</td>
+        })
+        }
         <td>
            <input type="button" title="Сохранить"  value="Сохранить" onClick={() =>   Actions.SetIndexSave(1) }/>
         </td>  
